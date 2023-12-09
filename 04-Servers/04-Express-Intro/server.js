c45ef3c4937adac7712d0dbea986b62f85c2fd5f
aymanab5 <aymansce@gmail.com>
2023-12-09 05:14:27 +0200

    Express Intro

--- 04-Servers/04-Express-Intro/server.js
+++ 04-Servers/04-Express-Intro/server.js
@@ -1,9 +1,48 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-app.get("/", function (request, response) {
-  console.log("Someone hass come into the server. Brace yourselves.");
-  response.send("Ending the cycle, thanks for visiting");
+const data = {
+  8112: {
+    title: "Name of the Wind",
+    author: "Patrick Rothfuss",
+  },
+  9121: {
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+  },
+  1081: {
+    title: "The Giver",
+    author: "Lois Lowry",
+  },
+};
+
+app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "node_modules")));
+
+app.get("/books/:bookID", function (request, response) {
+  let bookID = request.params.bookID;
+  response.send(data[bookID]);
+});
+
+app.get("/error", function (request, response) {
+  console.log("Someone has come into the server. Brace yourselves.");
+  //response.send("Ending the cycle, thanks for visiting");
+  //response.sendFile(path.join(__dirname, "error.html"));
+  response.sendFile(path.join(__dirname, "dist", "error.html"));
+});
+
+app.get("/maps", function (request, response) {
+  response.send("Here's some stuff related to maps");
+});
+
+app.get("/shoobi", function (request, response) {
+  response.send("This here is the shoobi *route*");
+});
+
+app.get("/routeWithOptionalParameters", (request, response) => {
+  let params = request.query;
+  response.send(params);
 });
 
 const port = 3000;
@@ -12,3 +51,6 @@ app.listen(port, function () {
 });
 
 //http://localhost:3000/
+//http://localhost:3000/routeWithOptionalParameters/?name=Ayman&car=BMW
+
+//http://localhost:3000/details/?zipcode=6863155&city=Ankh Morpork&middleName=Wilfred
