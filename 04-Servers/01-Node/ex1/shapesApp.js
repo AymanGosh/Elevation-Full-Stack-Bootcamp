f929bb3b2cfa2eebb3332b312220749edd940971
aymanab5 <aymansce@gmail.com>
2023-12-18 00:45:00 +0200

    MongoDB

--- 04-Servers/01-Node/ex1/shapesApp.js
+++ 04-Servers/01-Node/ex1/shapesApp.js
@@ -1,6 +1,6 @@
 const circleUtilities = require("./circleUtils");
 console.log(circleUtilities);
 //{ pi: 3.14159, calcCircleArea: [Function] }
-
+console.log(circleUtilities.calcCircleArea(10));
 console.log(title);
 //Circle Utility
