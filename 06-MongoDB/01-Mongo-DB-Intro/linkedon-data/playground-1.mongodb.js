b393a2ae94b4e048965bf1887ab13e73a2eab70a
aymangosh <aymansce@gmail.com>
2023-12-28 21:25:58 +0200

    Mongod DB intro

--- 06-MongoDB/01-Mongo-DB-Intro/linkedon-data/playground-1.mongodb.js
+++ 06-MongoDB/01-Mongo-DB-Intro/linkedon-data/playground-1.mongodb.js
@@ -2,3 +2,22 @@
 use('test-db');
 
 db.linkedon.find({}) //or whichever operation
+
+//db.linkedon.countDocuments({ salary: { $gt: 25000 } })
+
+db.linkedon.find(
+    { },
+    { firstName: 1, salary: 1, _id: 1 } // Projection to include only firstName and salary
+  )
+  .sort({ salary: -1 }) // Sort by salary in descending order
+  .limit(3) // Limit to top 3 earners
+  
+  db.linkedon.countDocuments({
+    "currentCompany.name": "Walmart",
+    "salary": { $gte: 7000 }
+  })
+  
+
+
+
+  
