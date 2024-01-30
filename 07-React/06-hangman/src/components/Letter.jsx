dc6478931b1aae621a629a0454bd59a0f93703e6
aymanab5 <aymansce@gmail.com>
2024-01-30 17:45:41 +0200

    update hangman2

--- 07-React/06-hangman/src/components/Letter.jsx
+++ 07-React/06-hangman/src/components/Letter.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from "react";
 
-function Letter({Letter,className,onClickEvent}) {
-    
-    
-    return (
-        <span onClick={()=>{onClickEvent(Letter)}} className={className}>{Letter}</span>
+function Letter({ Letter, className = "enable", onClickEvent }) {
+  const handelClick = () => {
+    if (onClickEvent) {
+      onClickEvent(Letter);
+    }
+  };
 
-    )
+  return (
+    <span onClick={handelClick} className={className}>
+      {Letter}
+    </span>
+  );
 }
 
-export default Letter
-
+export default Letter;
--- 07-React/06-hangman/src/components/Solution.jsx
+++ 07-React/06-hangman/src/components/Solution.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-
-function Solution({solution,SEARCHEN_WORD}) {
-    const searchenWord=[...SEARCHEN_WORD]
-    return (
-        <>{searchenWord.map(l=>{
-            return solution.has(l)?l+" ":"_ ";
-        })}</>
-    )
+import React from "react";
+import Letter from "./Letter";
+function Solution({ solution, SEARCHEN_WORD }) {
+  const searchenWord = [...SEARCHEN_WORD];
+  return (
+    <>
+      {searchenWord.map((l, index) => {
+        return <Letter key={index} Letter={solution.has(l) ? l + " " : "_ "} />;
+      })}
+    </>
+  );
 }
 
-export default Solution
+export default Solution;
