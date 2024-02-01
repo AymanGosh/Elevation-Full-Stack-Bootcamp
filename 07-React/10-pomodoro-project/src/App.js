571c3352b944d3c43f477103ea9788b2772b2a9d
aymanab5 <aymansce@gmail.com>
2024-02-01 15:55:54 +0200

    Fix useState bug in App component

---
+++ 07-React/10-pomodoro-project/src/App.js
@@ -0,0 +1,85 @@
+import "./App.css";
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Clock from "./components/Clock";
+import ActionRow from "./components/ActionRow";
+
+function App() {
+  const [minutes, setMinutes] = useState("25");
+  const [seconds, setSeconds] = useState("00");
+  const [timer, setTimer] = useState(null);
+  const [isWorkMode, setIsWorkMode] = useState(true);
+  const [isActive, setIsActive] = useState(false);
+
+  const start = () => {
+    setTimer(setInterval(tick, 1000));
+    setIsActive(true);
+  };
+
+  const pause = () => {
+    clearInterval(timer);
+    setIsActive(false);
+  };
+
+  const reset = () => {
+    pause();
+    if (isWorkMode) {
+      setMinutes("25");
+      setSeconds("00");
+    } else {
+      setMinutes("01");
+      setSeconds("00");
+    }
+  };
+
+  const finished = () => {
+    pause();
+    if (isWorkMode) {
+      setIsWorkMode(false);
+      setMinutes("01");
+      setSeconds("00");
+    } else {
+      setIsWorkMode(true);
+      setMinutes("25");
+      setSeconds("00");
+    }
+  };
+
+  const changeMode = (mode) => {
+    pause();
+    if (mode === "work") {
+      setIsWorkMode(true);
+      setMinutes("25");
+      setSeconds("00");
+    } else {
+      setIsWorkMode(false);
+      setMinutes("01");
+      setSeconds("00");
+    }
+  };
+
+  const tick = () => {
+    setSeconds((prevSeconds) => {
+      if (prevSeconds === "00" || prevSeconds === "0") {
+        setMinutes((minutes) => String(Number(minutes) - 1));
+        return "59";
+      } else {
+        return String(Number(prevSeconds) - 1);
+      }
+    });
+  };
+
+  return (
+    <div className="App">
+      <Navbar changeMode={changeMode} />
+      <Clock minutes={minutes} seconds={seconds} />
+      <ActionRow
+        activateAction={isActive ? pause : start}
+        resetClicked={reset}
+        currentAction={isActive ? "Pause" : "Start"}
+      />
+    </div>
+  );
+}
+
+export default App;
